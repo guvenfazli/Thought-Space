c25fecab2f75d5c05c0fff0db23a27087a971975
Güven Fazlı <guven.fazli@gmail.com>
2024-04-25 22:18:27 +0300

    LogIn section is done

--- tought-space/Components/LogInSignIn/LogIn.jsx
+++ tought-space/Components/LogInSignIn/LogIn.jsx
@@ -1,11 +1,38 @@
+import { useState, useRef } from "react"
+import { logInAccount } from "@/Utils/UserAuth"
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from "@/app/firebaseConfig";
+
+
 export default function LogIn() {
+  const [userLogInInfo, setUserLogInInfo] = useState({
+    email: "",
+    password: ""
+  })
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  function getUserInfo(field, ref) {
+    setUserLogInInfo((prev) => {
+      let info = { ...prev, [field]: ref.current.value }
+      console.log(info)
+      return info
+    })
+  }
+
+  async function logIn(userInfo, e) {
+    e.preventDefault()
+    await logInAccount(userInfo)
+
+  }
 
   return (
-    <form className="flex flex-col p-1 gap-y-4">
+    <form onSubmit={(e) => logIn(userLogInInfo, e)} className="flex flex-col p-1 gap-y-4">
       <label htmlFor="email" className="text-2xl">Email</label>
-      <input type="email" name="email" className="text-black p-2 rounded-xl" placeholder="Email" />
+      <input ref={emailRef} onChange={() => getUserInfo("email", emailRef)} type="email" name="email" className="text-black p-2 rounded-xl" placeholder="Email" />
       <label htmlFor="password" className="text-2xl">Password</label>
-      <input type="password" name="password" className="text-black p-2 rounded-xl" placeholder="Password" />
+      <input ref={passwordRef} onChange={() => getUserInfo("password", passwordRef)} type="password" name="password" className="text-black p-2 rounded-xl" placeholder="Password" />
       <div className="flex justify-center items-center">
         <button type="submit" className="py-2 px-8 border-2 rounded-2xl duration-150 ease-in-out bg-blue-800 hover:bg-blue-500 text-white font-bold">Log In</button>
       </div>
---
+++ tought-space/Utils/UserAuth.js
@@ -0,0 +1,22 @@
+import { auth } from "../app/firebaseConfig";
+import { db } from "../app/firebaseConfig";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
+import { collection, setDoc, addDoc, doc, getDoc, updateDoc } from "firebase/firestore";
+
+async function refreshToken(userRef, user) {
+  const getUser = await getDoc(userRef).then((userData) => userData.data())
+  await updateDoc(userRef, {
+    ...getUser,
+    token: user.accessToken
+  })
+  localStorage.setItem('token', user.accessToken)
+  localStorage.setItem('user', JSON.stringify({ id: user.uid }))
+
+}
+
+export async function logInAccount(userInfo) {
+  const userCredential = await signInWithEmailAndPassword(auth, userInfo.email, userInfo.password)
+  const user = userCredential.user
+  const userRef = doc(db, "userList", `${user.uid}`)
+  await refreshToken(userRef, user)
+}
